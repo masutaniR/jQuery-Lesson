12eb00acee14d26a6c412738f65025638fec687e
masutaniR <popohu87@gmail.com>
2021-01-10 18:03:36 +0900

    [Finish] JS-5

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(function(){
-  $('.box1').css({
-    'background-color': '#0000ff',
-    'height': '100px'
+  $('.box1').slideDown(5000, function (){
+    $('.box1').css({
+      'background-color': '#0000ff',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(5000);
   });
 });
